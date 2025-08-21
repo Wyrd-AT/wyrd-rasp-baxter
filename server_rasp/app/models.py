7a6b856517402c9f459ba114a3ae0676c00fd595
icaro-ms <icaro.sicchieri@wyrd.com.br>
2025-08-21 17:22:46 -0300

    nova db

--- server_rasp/app/models.py
+++ server_rasp/app/models.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 import logging
 logger = logging.getLogger(__name__)
 
-DATABASE_URL = "sqlite:///./base_wh.db"
+DATABASE_URL = "sqlite:///./base_wh_ff.db"
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
